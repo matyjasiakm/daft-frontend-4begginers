64b81b6e74b94aac7928f0feb451c61ad572f452
Mateusz Matyjasiak <m.matyjasiak1@wp.pl>
2022-05-23 14:46:10 +0200

    feat(InputForm): Add input form for username [3-20 characters]

---
+++ src/InputForm/InputForm.tsx
@@ -0,0 +1,76 @@
+import {useState, useEffect, useCallback} from "react";
+import './InputForm.css';
+
+const storageFormKey = 'inpVal'
+
+export const InputForm = () => {
+    const [dispErrorMsg, setDispErrorMsg] = useState<boolean>(false)
+    const [errMsg, setErrMsg] = useState<string>('')
+    const [inputVal, setInputVal] = useState<string>('')
+
+    useEffect(() => {
+        const delay = setTimeout(() => {
+            console.log("time")
+        }, 3000)
+        return () => clearTimeout(delay)
+    }, [dispErrorMsg])
+
+    //Handler for input value
+    const inputHandler = (event: any) => {
+        const msgLength = event.target.value.length;
+        setInputVal(event.target.value)
+
+        if (msgLength > 20) {
+            setDispErrorMsg(true)
+            setErrMsg('Za długa nazwa (Podaj: 3-20 znaków)')
+        } else if (msgLength < 3) {
+            setDispErrorMsg(true)
+            setErrMsg('Za krótka nazwa (Podaj: 3-20 znaków)')
+
+        } else {
+            setDispErrorMsg(false)
+            setErrMsg('')
+        }
+
+
+    }
+    //Submit button
+    const onClickSubmitButton = useCallback(() => {
+        console.log('click')
+        if (!dispErrorMsg && inputVal.length <= 20 && inputVal.length >= 3) {
+            localStorage.setItem(storageFormKey, inputVal)
+        }
+    }, [inputVal, dispErrorMsg])
+
+    //Load button
+    const onClickLoadButton = useCallback(() => {
+        console.log('clickLoad')
+        loadValue()
+        setDispErrorMsg(false)
+        setErrMsg('')
+    }, [])
+
+    //Load value form storage
+    const loadValue = () => {
+        const lcStorageData = localStorage.getItem(storageFormKey)
+        if (!!lcStorageData) {
+            setInputVal(lcStorageData)
+        }
+    }
+    //Load value if is in storage
+    useEffect(() => {
+        loadValue()
+    }, [])
+
+    return <div style={{display: "flex", flexDirection: 'column'}}>
+        <div>Wprowadź nazwę użytkownika:</div>
+        {dispErrorMsg && <div style={{color: 'red'}}>{errMsg}</div>}
+        <input className="Input-text-form" type="text" onChange={inputHandler} value={inputVal} onInput={inputHandler}/>
+        <div style={{display: "flex", flexDirection: "row", marginLeft: 'auto', marginRight: 'auto'}}>
+            <button className="Submit-button" onClick={onClickSubmitButton}
+                    disabled={dispErrorMsg || inputVal.length === 0}>Zapisz
+            </button>
+            <button className="Submit-button" onClick={onClickLoadButton}>Wczytaj</button>
+        </div>
+    </div>
+}
